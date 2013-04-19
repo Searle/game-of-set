e8bd64a0628bb718322036cb13a82a4db5b880ea
stephan hantigk <s.hantigk@runlevel3.de>
2013-04-19 13:28:22 +0200

    fixed empty cards dietrichfied code added fourth row

--- meteor/game-of-set.js
+++ meteor/game-of-set.js
@@ -4,17 +4,26 @@ var loadGame= function() {
     return Games.findOne();
 };
 
-var Card= function(num, row, col) {
+var Card= function( num, row, col ) {
 
     this.row= row;
     this.col= col;
 
-    this.setFace= function(num) {
-        this.num=    num;
-        this.fill=   num % 3; num = Math.floor(num / 3);
-        this.motive= num % 3; num = Math.floor(num / 3);
-        this.color=  num % 3; num = Math.floor(num / 3);
-        this.count=  num % 3 + 1;
+    this.setFace= function( num ) {
+        this.num= num;
+
+        if ( typeof num === 'number' ) {
+            this.fill=   num % 3; num = Math.floor(num / 3);
+            this.motive= num % 3; num = Math.floor(num / 3);
+            this.color=  num % 3; num = Math.floor(num / 3);
+            this.count=  num % 3 + 1;
+        }
+        else {
+            this.fill=   0;
+            this.motive= 0;
+            this.color=  0;
+            this.count=  0;
+        }
 
         this.cssX= this.fill * 96 + this.color * 32;
         this.cssY= this.motive * 64;
@@ -22,24 +31,25 @@ var Card= function(num, row, col) {
 
     this.setFace(num);
 }
+
 Card.prototype.key= function() {
     return this.row + "-" + this.col;
 };
 
-var cardsMatch= function(c1, c2, c3) {
+var cardsMatch= function( c1, c2, c3 ) {
     var matches= true;
-    ['fill', 'motive', 'color', 'count'].forEach(function(type) {
+
+    ['fill', 'motive', 'color', 'count'].forEach(function( type ) {
         t1= c1[type];
         t2= c2[type];
         t3= c3[type];
         matches= matches && ((t1 === t2 && t1 === t3) || (t1 !== t2 && t2 !== t3 && t1 !== t3));
-    })
+    });
     return matches;
 }
 
 var initGame= function() {
     var game= loadGame();
-    var round= 0;
 
     if ( game ) {
         round= game.round;
@@ -54,15 +64,15 @@ var initGame= function() {
     for (var j, x, i = cardStack.length; i; j = parseInt(Math.random() * i), x = cardStack[--i], cardStack[i] = cardStack[j], cardStack[j] = x);
 
     var visibleCards= [];
-    for (var r= 0; r < 3; r++) {
+    for ( var r= 0; r < 4; r++ ) {
         var row= [];
         visibleCards.push(row);
-        for (var c= 0; c < 4; c++) {
+        for ( var c= 0; c < 4; c++ ) {
             row.push(cardStack.shift());
         }
     }
 
-    Games.insert({ visibleCards: visibleCards, cardStack: cardStack, round: round, });
+    Games.insert({ visibleCards: visibleCards, cardStack: cardStack, round: 0, });
 };
 
 if ( Meteor.isClient ) {
@@ -74,7 +84,7 @@ if ( Meteor.isClient ) {
     var buildCards= function() {
         var game= loadGame();
 
-        if ( !game || !("visibleCards" in game)) {
+        if ( !game || !("visibleCards" in game) ) {
             cardSet= [];
             return cardSet;
         }
@@ -89,7 +99,7 @@ if ( Meteor.isClient ) {
                     return {
                         card: new Card(num, r, c),
                     };
-                })
+                }),
             };
         });
 
@@ -119,7 +129,7 @@ if ( Meteor.isClient ) {
 
     Template.card.class= function() {
         var card= this.card;
-        return (Session.get('selectedCards') || []).filter(function(c) {
+        return (Session.get('selectedCards') || []).filter(function( c ) {
             return c.num === card.num;
         }).length ? "selected" : "";
     }
@@ -128,7 +138,7 @@ if ( Meteor.isClient ) {
         var elems= [];
         var card= this.card;
         var cardElem= {cssX: card.cssX, cssY: card.cssY};
-        for (var i= 0; i < card.count; i++) {
+        for ( var i= 0; i < card.count; i++ ) {
             elems.push(cardElem);
         }
         return elems;
@@ -137,11 +147,13 @@ if ( Meteor.isClient ) {
     Template.card.events({
         'click .card': function() {
             var card= this.card;
-            var oldSelected= selectedCards;
-            selectedCards= selectedCards.filter(function(c) {
+            if ( typeof card.num !== 'number' ) return;
+
+            var oldSelectedLength= selectedCards.length;
+            selectedCards= selectedCards.filter(function( c ) {
                 return c.num !== card.num;
             });
-            if (oldSelected.length != selectedCards.length) {
+            if ( oldSelectedLength != selectedCards.length ) {
                 Session.set('selectedCards', selectedCards)
                 return;
             }
@@ -151,7 +163,7 @@ if ( Meteor.isClient ) {
 
             if ( selectedCards.length < 3 ) return;
 
-            if (cardsMatch(selectedCards[0], selectedCards[1], selectedCards[2])) {
+            if ( cardsMatch(selectedCards[0], selectedCards[1], selectedCards[2]) ) {
                 console.log("MATCH")
                 var game= loadGame();
 
@@ -161,7 +173,7 @@ if ( Meteor.isClient ) {
                     game.visibleCards[card.row][card.col]= face;
                 });
                 game.round++;
-                Games.update(game._id, { $set: { visibleCards: game.visibleCards, cardStack: game.cardStack, round: game.round, }});
+                Games.update(game._id, { $set: { visibleCards: game.visibleCards, cardStack: game.cardStack, round: game.round, } });
             }
 
             selectedCards= [];
@@ -172,14 +184,14 @@ if ( Meteor.isClient ) {
 
 if ( Meteor.isServer ) {
     Games.allow({
-        update: function(userId, doc, fieldNames, modifier) {
+        update: function( userId, doc, fieldNames, modifier ) {
             var nextRound= doc.round + 1;
-            return modifier['$set'].round === nextRound;
+            return modifier && modifier['$set'] && modifier['$set'].round === nextRound;
         },
-        insert: function(userId, doc) {
+        insert: function( userId, doc ) {
             return true;
         },
-        remove: function(userId, doc) {
+        remove: function( userId, doc ) {
             return true;
         },
     })
